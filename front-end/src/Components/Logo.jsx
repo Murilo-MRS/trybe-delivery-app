4f1068e4755e4b5150cd4e15559a05fd747e6032
Felipe Pinto <felipe14062000@gmail.com>
2023-04-05 13:20:15 -0300

    feat(front-end): criação do component Logo

---
+++ front-end/src/Components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Logo() {
+  return (
+    <div>
+      <img src="http://localhost:3001/images/Logo.svg" alt="Logo" width={ 150 } />
+    </div>
+  );
+}
+
+export default Logo;
--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { getUser, logout } from '../Utils/LocalStorage';
+import Logo from './Logo';
 
 export default function Navbar() {
   const [userName, setUserName] = useState('');
@@ -14,6 +15,7 @@ export default function Navbar() {
 
   return (
     <nav>
+      <Logo />
       {userRole === 'customer' && (
         <NavLink
           to="/customer/products"
