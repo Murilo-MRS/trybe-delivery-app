1fccebd0275fe0b2f8adfa95a6960dd813150381
Murilo-MRS <murilosantana1993@gmail.com>
2023-04-11 18:05:18 -0300

    test(back): add teste de integracão

--- back-end/src/tests/integration/Product.test.js
+++ back-end/src/tests/integration/Product.test.js
@@ -1,8 +1,8 @@
 const chai = require('chai');
 const sinon = require('sinon');
 const chaiHttp = require('chai-http');
-
 const { Product } = require('../../database/models');
+const { productResponseMock, productIdResponse } = require('./mocks/product.mock');
 
 const { expect } = chai;
 
@@ -11,24 +11,37 @@ chai.use(chaiHttp);
 const app = require('../../api/app');
 
 describe('Teste rota Products', function () {
-  const productMock = {
-    id: 1,
-    name: 'Product Test',
-    price: 20.99,
-    urlImage: 'teste',
-  };
-
-  before(async function () {
-    sinon
-      .stub(Product, 'findOne')
-      .resolves(productMock);
+  afterEach(function () {
+    sinon.restore();
   });
 
   it('SUCESSO - retorna todos produtos', async function () {
     // Arrange
+    sinon.stub(Product, 'findAll').resolves(productResponseMock);
     // Act
-    const chaiHttpResponse = await chai.request(app).get('/customer/products').send({});
+    const chaiHttpResponse = await chai.request(app).get('/customer/products');
     // Assert
     expect(chaiHttpResponse).to.have.status(200);
+    expect(chaiHttpResponse.body).to.be.deep.equal(productResponseMock);
+  });
+
+  it('SUCESSO - retorna um produto por id', async function () {
+    // Arrange
+    sinon.stub(Product, 'findOne').resolves(productIdResponse);
+    // Act
+    const chaiHttpResponse = await chai.request(app).get('/customer/products/1');
+    // Assert
+    expect(chaiHttpResponse).to.have.status(200);
+    expect(chaiHttpResponse.body).to.be.deep.equal(productIdResponse);
+  });
+
+  it('EXCESSÃO - produto com id inexistente', async function () {
+    // Arrange
+    sinon.stub(Product, 'findOne').resolves(null);
+    // Act
+    const chaiHttpResponse = await chai.request(app).get('/customer/products/1');
+    // Assert
+    expect(chaiHttpResponse).to.have.status(404);
+    expect(chaiHttpResponse.body).to.be.deep.equal({ message: 'Not found' });
   });
 });
