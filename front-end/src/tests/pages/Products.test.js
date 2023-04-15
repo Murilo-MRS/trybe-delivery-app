1f1ad453d3b84a2c77054359cc3f3cda89031e4e
Murilo-MRS <murilosantana1993@gmail.com>
2023-04-15 10:54:26 -0300

    test(front): descomentando testes

--- front-end/src/tests/pages/Products.test.js
+++ front-end/src/tests/pages/Products.test.js
@@ -1,47 +1,47 @@
-// /* eslint-disable max-len */
-// import { screen, waitFor } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import App from '../../App';
-// import renderWithRouter from './utils/renderWithRouter';
-// import allProductsMock from '../mocks/allProducts.mock';
+/* eslint-disable max-len */
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../../App';
+import renderWithRouter from './utils/renderWithRouter';
+import allProductsMock from '../mocks/allProducts.mock';
 
-// describe('Test: login /seller', () => {
-//   const emailDataTestId = 'common_login__input-email';
-//   const loginBtnDataTestId = 'common_login__button-login';
-//   const passwordDataTestId = 'common_login__input-password';
-//   const customerEmail = 'zebirita@email.com';
-//   const customerPassword = '$#zebirita#$';
-//   const PRODUCT_LENGTH = 11;
+describe('Test: login /seller', () => {
+  const emailDataTestId = 'common_login__input-email';
+  const loginBtnDataTestId = 'common_login__button-login';
+  const passwordDataTestId = 'common_login__input-password';
+  const customerEmail = 'zebirita@email.com';
+  const customerPassword = '$#zebirita#$';
+  const PRODUCT_LENGTH = 11;
 
-//   const TOKEN_CUSTOMER = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo5LCJuYW1lIjoiQ2xpZW50ZSBaw6kgQmlyaXRhIiwiZW1haWwiOiJ6ZWJpcml0YUBlbWFpbC5jb20iLCJyb2xlIjoiY3VzdG9tZXIifSwiaWF0IjoxNjgxMzI3NTIxLCJleHAiOjE2ODE5MzIzMjF9.5-jg8opGN28n8MrzjFCHKQPsqd3eqQX9_hHNVpqpS8o';
+  const TOKEN_CUSTOMER = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo5LCJuYW1lIjoiQ2xpZW50ZSBaw6kgQmlyaXRhIiwiZW1haWwiOiJ6ZWJpcml0YUBlbWFpbC5jb20iLCJyb2xlIjoiY3VzdG9tZXIifSwiaWF0IjoxNjgxMzI3NTIxLCJleHAiOjE2ODE5MzIzMjF9.5-jg8opGN28n8MrzjFCHKQPsqd3eqQX9_hHNVpqpS8o';
 
-//   beforeEach(() => {
-//     window.localStorage.clear();
-//   });
+  beforeEach(() => {
+    window.localStorage.clear();
+  });
 
-//   afterEach(() => {
-//     jest.restoreAllMocks();
-//   });
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
 
-//   it('1 - Test: /customer/products aparecem os produtos', async () => {
-//     jest.spyOn(global, 'fetch')
-//       .mockImplementationOnce(() => Promise.resolve({
-//         json: () => Promise.resolve({ token: TOKEN_CUSTOMER }),
-//       }))
-//       .mockImplementationOnce(() => Promise.resolve({
-//         json: () => Promise.resolve(allProductsMock),
-//       }));
+  it('1 - Test: /customer/products aparecem os produtos', async () => {
+    jest.spyOn(global, 'fetch')
+      .mockImplementationOnce(() => Promise.resolve({
+        json: () => Promise.resolve({ token: TOKEN_CUSTOMER }),
+      }))
+      .mockImplementationOnce(() => Promise.resolve({
+        json: () => Promise.resolve(allProductsMock),
+      }));
 
-//     renderWithRouter(<App />);
-//     const email = screen.getByTestId(emailDataTestId);
-//     const password = screen.getByTestId(passwordDataTestId);
-//     const loginButton = screen.getByTestId(loginBtnDataTestId);
+    renderWithRouter(<App />);
+    const email = screen.getByTestId(emailDataTestId);
+    const password = screen.getByTestId(passwordDataTestId);
+    const loginButton = screen.getByTestId(loginBtnDataTestId);
 
-//     userEvent.type(email, customerEmail);
-//     userEvent.type(password, customerPassword);
-//     userEvent.click(loginButton);
+    userEvent.type(email, customerEmail);
+    userEvent.type(password, customerPassword);
+    userEvent.click(loginButton);
 
-//     const addBtn = await waitFor(() => screen.getByTestId('customer_products__button-card-add-item-1'));
-//     userEvent.click(addBtn);
-//   });
-// });
+    const addBtn = await waitFor(() => screen.getByTestId('customer_products__button-card-add-item-1'));
+    userEvent.click(addBtn);
+  });
+});
--- front-end/src/tests/pages/Register.test.js
+++ front-end/src/tests/pages/Register.test.js
@@ -1,86 +1,85 @@
-// /* eslint-disable max-len */
-// import { screen, waitFor } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import App from '../../App';
-// import renderWithRouter from './utils/renderWithRouter';
-// import allProductsMock from '../mocks/allProducts.mock';
-// import axios from 'axios';
+/* eslint-disable max-len */
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../../App';
+import renderWithRouter from './utils/renderWithRouter';
+import allProductsMock from '../mocks/allProducts.mock';
 
-// describe('Tests referring to the register page.', () => {
-//   const nameDataTestId = 'common_register__input-name';
-//   const emailDataTestId = 'common_register__input-email';
-//   const passwordDataTestId = 'common_register__input-password';
-//   const registerBtnDataTestId = 'common_register__button-register';
-//   const validName = 'João da Silva';
-//   const validEmail = 'joao@email.com';
-//   const validPassword = 'joao123';
-//   const alreadyUsedEmail = 'zebirita@email.com';
+describe('Tests referring to the register page.', () => {
+  const nameDataTestId = 'common_register__input-name';
+  const emailDataTestId = 'common_register__input-email';
+  const passwordDataTestId = 'common_register__input-password';
+  const registerBtnDataTestId = 'common_register__button-register';
+  const validName = 'João da Silva';
+  const validEmail = 'joao@email.com';
+  const validPassword = 'joao123';
+  const alreadyUsedEmail = 'zebirita@email.com';
 
-//   const TOKEN_CUSTOMER = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo5LCJuYW1lIjoiQ2xpZW50ZSBaw6kgQmlyaXRhIiwiZW1haWwiOiJ6ZWJpcml0YUBlbWFpbC5jb20iLCJyb2xlIjoiY3VzdG9tZXIifSwiaWF0IjoxNjgxMzI3NTIxLCJleHAiOjE2ODE5MzIzMjF9.5-jg8opGN28n8MrzjFCHKQPsqd3eqQX9_hHNVpqpS8o';
+  const TOKEN_CUSTOMER = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo5LCJuYW1lIjoiQ2xpZW50ZSBaw6kgQmlyaXRhIiwiZW1haWwiOiJ6ZWJpcml0YUBlbWFpbC5jb20iLCJyb2xlIjoiY3VzdG9tZXIifSwiaWF0IjoxNjgxMzI3NTIxLCJleHAiOjE2ODE5MzIzMjF9.5-jg8opGN28n8MrzjFCHKQPsqd3eqQX9_hHNVpqpS8o';
 
-//   beforeEach(() => {
-//     window.localStorage.clear();
-//   });
+  beforeEach(() => {
+    window.localStorage.clear();
+  });
 
-//   afterEach(() => {
-//     jest.restoreAllMocks();
-//   });
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
 
-//   it('1 - Test: register with valid data', async () => {
-//     jest.spyOn(global, 'fetch')
-//       .mockImplementationOnce(() => Promise.resolve({
-//         json: () => Promise.resolve({ token: TOKEN_CUSTOMER }),
-//       }))
-//       .mockImplementationOnce(() => Promise.resolve({
-//         json: () => Promise.resolve(allProductsMock),
-//       }));
-//     const { history } = renderWithRouter(<App />, { initialEntries: ['/register'] });
+  it('1 - Test: register with valid data', async () => {
+    jest.spyOn(global, 'fetch')
+      .mockImplementationOnce(() => Promise.resolve({
+        json: () => Promise.resolve({ token: TOKEN_CUSTOMER }),
+      }))
+      .mockImplementationOnce(() => Promise.resolve({
+        json: () => Promise.resolve(allProductsMock),
+      }));
+    const { history } = renderWithRouter(<App />, { initialEntries: ['/register'] });
 
-//     const buttonRegister = screen.getByTestId(registerBtnDataTestId);
-//     const inputName = screen.getByTestId(nameDataTestId);
-//     const inputEmail = screen.getByTestId(emailDataTestId);
-//     const inputPassword = screen.getByTestId(passwordDataTestId);
+    const buttonRegister = screen.getByTestId(registerBtnDataTestId);
+    const inputName = screen.getByTestId(nameDataTestId);
+    const inputEmail = screen.getByTestId(emailDataTestId);
+    const inputPassword = screen.getByTestId(passwordDataTestId);
 
-//     userEvent.type(inputName, validName);
-//     userEvent.type(inputEmail, validEmail);
-//     userEvent.type(inputPassword, validPassword);
+    userEvent.type(inputName, validName);
+    userEvent.type(inputEmail, validEmail);
+    userEvent.type(inputPassword, validPassword);
 
-//     expect(buttonRegister).toBeEnabled();
+    expect(buttonRegister).toBeEnabled();
 
-//     userEvent.click(buttonRegister);
+    userEvent.click(buttonRegister);
 
-//     await waitFor(() => {
-//       expect(history.location.pathname).toBe('/customer/products');
-//     });
+    await waitFor(() => {
+      expect(history.location.pathname).toBe('/customer/products');
+    });
 
-//     const logoutBtn = screen.getByTestId('customer_products__element-navbar-link-logout');
-//     expect(logoutBtn).toBeInTheDocument();
+    const logoutBtn = screen.getByTestId('customer_products__element-navbar-link-logout');
+    expect(logoutBtn).toBeInTheDocument();
 
-//     userEvent.click(logoutBtn);
-//   });
+    userEvent.click(logoutBtn);
+  });
 
-//   it('2 - If error message show with login invalid user', async () => {
-//     const { history } = renderWithRouter(<App />, { initialEntries: ['/register'] });
+  it('2 - If error message show with login invalid user', async () => {
+    const { history } = renderWithRouter(<App />, { initialEntries: ['/register'] });
 
-//     const inputName = screen.getByTestId(nameDataTestId);
-//     const email = screen.getByTestId(emailDataTestId);
-//     const password = screen.getByTestId(passwordDataTestId);
-//     const buttonRegister = screen.getByTestId(registerBtnDataTestId);
+    const inputName = screen.getByTestId(nameDataTestId);
+    const email = screen.getByTestId(emailDataTestId);
+    const password = screen.getByTestId(passwordDataTestId);
+    const buttonRegister = screen.getByTestId(registerBtnDataTestId);
 
-//     userEvent.type(inputName, validName);
-//     userEvent.type(email, alreadyUsedEmail);
-//     userEvent.type(password, validPassword);
+    userEvent.type(inputName, validName);
+    userEvent.type(email, alreadyUsedEmail);
+    userEvent.type(password, validPassword);
 
-//     userEvent.click(buttonRegister);
+    userEvent.click(buttonRegister);
 
-//     await waitFor(() => {
-//       const { location: { pathname } } = history;
-//       expect(pathname).toBe('/register');
-//     });
+    await waitFor(() => {
+      const { location: { pathname } } = history;
+      expect(pathname).toBe('/register');
+    });
 
-//     const errorMsg = await waitFor(() => screen
-//       .getByTestId('common_register__element-invalid_register'));
+    const errorMsg = await waitFor(() => screen
+      .getByTestId('common_register__element-invalid_register'));
 
-//     expect(errorMsg).toBeInTheDocument();
-//   });
-// });
+    expect(errorMsg).toBeInTheDocument();
+  });
+});
