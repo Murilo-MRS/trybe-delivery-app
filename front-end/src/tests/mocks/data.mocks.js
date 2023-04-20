bd802ad7b34c38e761f5b206b8172ad7d5e5792b
Murilo-MRS <murilosantana1993@gmail.com>
2023-04-20 18:15:38 -0300

    test: criado dataMock diminuir linhas do arquivo

---
+++ front-end/src/tests/mocks/data.mocks.js
@@ -0,0 +1,50 @@
+/* eslint-disable max-len */
+const emailDataTestId = 'common_login__input-email';
+const loginBtnDataTestId = 'common_login__button-login';
+const registerBtnDataTestId = 'common_login__button-register';
+const passwordDataTestId = 'common_login__input-password';
+const customerEmail = 'zebirita@email.com';
+const customerPassword = '$#zebirita#$';
+const admEmail = 'adm@deliveryapp.com';
+const admPassword = '--adm2@21!!--';
+const sellerEmail = 'fulana@deliveryapp.com';
+const sellerPassword = 'fulana@123';
+const STATUS_OK = 200;
+const NOT_FOUND = 404;
+const loginEndpoint = 'http://localhost:3001/login';
+
+const TOKEN_CUSTOMER = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo5LCJuYW1lIjoiQ2xpZW50ZSBaw6kgQmlyaXRhIiwiZW1haWwiOiJ6ZWJpcml0YUBlbWFpbC5jb20iLCJyb2xlIjoiY3VzdG9tZXIifSwiaWF0IjoxNjgxMzI3NTIxLCJleHAiOjE2ODE5MzIzMjF9.5-jg8opGN28n8MrzjFCHKQPsqd3eqQX9_hHNVpqpS8o';
+const TOKEN_SELLER = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoyLCJuYW1lIjoiRnVsYW5hIFBlcmVpcmEiLCJlbWFpbCI6ImZ1bGFuYUBkZWxpdmVyeWFwcC5jb20iLCJyb2xlIjoic2VsbGVyIn0sImlhdCI6MTY4MTMyNzk5NCwiZXhwIjoxNjgxOTMyNzk0fQ.BaHuWKIaXOf-M4DRYAMT_Xr4t6UCsOF8iV95dwsz5U0';
+const TOKEN_ADMIN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJuYW1lIjoiRGVsaXZlcnkgQXBwIEFkbWluIiwiZW1haWwiOiJhZG1AZGVsaXZlcnlhcHAuY29tIiwicm9sZSI6ImFkbWluaXN0cmF0b3IifSwiaWF0IjoxNjgxMjM5MDUyLCJleHAiOjE2ODE4NDM4NTJ9.fB-uigOpqdz_Y3Enzmw_CVVl816ne529XJEBVhh8nAA';
+
+const nameDataTestId = 'common_register__input-name';
+const validName = 'João da Silva';
+const validEmail = 'joao@email.com';
+const validPassword = 'joao123';
+const alreadyUsedEmail = 'zebirita@email.com';
+const registerEndpoint = 'http://localhost:3001/login';
+
+export {
+  emailDataTestId,
+  loginBtnDataTestId,
+  registerBtnDataTestId,
+  passwordDataTestId,
+  customerEmail,
+  customerPassword,
+  admEmail,
+  admPassword,
+  sellerEmail,
+  sellerPassword,
+  STATUS_OK,
+  NOT_FOUND,
+  loginEndpoint,
+  TOKEN_CUSTOMER,
+  TOKEN_SELLER,
+  TOKEN_ADMIN,
+  nameDataTestId,
+  validName,
+  validEmail,
+  validPassword,
+  alreadyUsedEmail,
+  registerEndpoint,
+};
--- front-end/src/tests/pages/Products.test.js
+++ front-end/src/tests/pages/Products.test.js
@@ -4,6 +4,7 @@ import userEvent from '@testing-library/user-event';
 import App from '../../App';
 import renderWithRouter from './utils/renderWithRouter';
 import allProductsMock from '../mocks/allProducts.mock';
+import mockAxios from './utils/mockAxios';
 
 describe('Test: login /seller', () => {
   const emailDataTestId = 'common_login__input-email';
@@ -11,7 +12,7 @@ describe('Test: login /seller', () => {
   const passwordDataTestId = 'common_login__input-password';
   const customerEmail = 'zebirita@email.com';
   const customerPassword = '$#zebirita#$';
-  const PRODUCT_LENGTH = 11;
+  // const PRODUCT_LENGTH = 11;
 
   const TOKEN_CUSTOMER = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo5LCJuYW1lIjoiQ2xpZW50ZSBaw6kgQmlyaXRhIiwiZW1haWwiOiJ6ZWJpcml0YUBlbWFpbC5jb20iLCJyb2xlIjoiY3VzdG9tZXIifSwiaWF0IjoxNjgxMzI3NTIxLCJleHAiOjE2ODE5MzIzMjF9.5-jg8opGN28n8MrzjFCHKQPsqd3eqQX9_hHNVpqpS8o';
 
@@ -24,13 +25,11 @@ describe('Test: login /seller', () => {
   });
 
   it('1 - Test: /customer/products aparecem os produtos', async () => {
-    jest.spyOn(global, 'fetch')
-      .mockImplementationOnce(() => Promise.resolve({
-        json: () => Promise.resolve({ token: TOKEN_CUSTOMER }),
-      }))
-      .mockImplementationOnce(() => Promise.resolve({
-        json: () => Promise.resolve(allProductsMock),
-      }));
+    mockAxios
+      .onPost(loginEndpoint)
+      .replyOnce(STATUS_OK, { token: TOKEN_CUSTOMER })
+      .onGet('http://localhost:3001/customer/products/')
+      .replyOnce(STATUS_OK, { products: allProductsMock });
 
     renderWithRouter(<App />);
     const email = screen.getByTestId(emailDataTestId);
@@ -41,7 +40,21 @@ describe('Test: login /seller', () => {
     userEvent.type(password, customerPassword);
     userEvent.click(loginButton);
 
-    const addBtn = await waitFor(() => screen.getByTestId('customer_products__button-card-add-item-1'));
-    userEvent.click(addBtn);
+    const addBtn1 = await waitFor(() => screen.getByTestId('customer_products__button-card-add-item-1'));
+    const removeBtn1 = await waitFor(() => screen.getByTestId('customer_products__button-card-rm-item-1'));
+    const addBtn2 = await waitFor(() => screen.getByTestId('customer_products__button-card-add-item-2'));
+    const removeBtn2 = await waitFor(() => screen.getByTestId('customer_products__button-card-rm-item-2'));
+
+    expect(addBtn1).toBeInTheDocument();
+    expect(removeBtn1).toBeInTheDocument();
+    expect(removeBtn1).toBeDisabled();
+
+    userEvent.click(addBtn1);
+    expect(removeBtn1).toBeEnabled();
+    userEvent.click(removeBtn1);
+
+    userEvent.click(addBtn2);
+    expect(removeBtn2).toBeEnabled();
+    userEvent.click(removeBtn2);
   });
 });
