e277e80e615e87bb97e629ef006828c971d446d3
Murilo-MRS <murilosantana1993@gmail.com>
2023-04-20 18:54:18 -0300

    test: testado fluxo de checkout

--- front-end/src/tests/pages/Products.test.js
+++ front-end/src/tests/pages/Products.test.js
@@ -5,17 +5,19 @@ import App from '../../App';
 import renderWithRouter from './utils/renderWithRouter';
 import allProductsMock from '../mocks/allProducts.mock';
 import mockAxios from './utils/mockAxios';
+import {
+  STATUS_OK,
+  TOKEN_CUSTOMER,
+  checkoutBtnDataTestId,
+  customerEmail,
+  customerPassword,
+  emailDataTestId,
+  loginBtnDataTestId,
+  loginEndpoint,
+  passwordDataTestId,
+} from '../mocks/data.mocks';
 
 describe('Test: login /seller', () => {
-  const emailDataTestId = 'common_login__input-email';
-  const loginBtnDataTestId = 'common_login__button-login';
-  const passwordDataTestId = 'common_login__input-password';
-  const customerEmail = 'zebirita@email.com';
-  const customerPassword = '$#zebirita#$';
-  // const PRODUCT_LENGTH = 11;
-
-  const TOKEN_CUSTOMER = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo5LCJuYW1lIjoiQ2xpZW50ZSBaw6kgQmlyaXRhIiwiZW1haWwiOiJ6ZWJpcml0YUBlbWFpbC5jb20iLCJyb2xlIjoiY3VzdG9tZXIifSwiaWF0IjoxNjgxMzI3NTIxLCJleHAiOjE2ODE5MzIzMjF9.5-jg8opGN28n8MrzjFCHKQPsqd3eqQX9_hHNVpqpS8o';
-
   beforeEach(() => {
     window.localStorage.clear();
   });
@@ -43,7 +45,7 @@ describe('Test: login /seller', () => {
     const addBtn1 = await waitFor(() => screen.getByTestId('customer_products__button-card-add-item-1'));
     const removeBtn1 = await waitFor(() => screen.getByTestId('customer_products__button-card-rm-item-1'));
     const addBtn2 = await waitFor(() => screen.getByTestId('customer_products__button-card-add-item-2'));
-    const removeBtn2 = await waitFor(() => screen.getByTestId('customer_products__button-card-rm-item-2'));
+    const checkouBtn = screen.getByTestId(checkoutBtnDataTestId);
 
     expect(addBtn1).toBeInTheDocument();
     expect(removeBtn1).toBeInTheDocument();
@@ -53,8 +55,11 @@ describe('Test: login /seller', () => {
     expect(removeBtn1).toBeEnabled();
     userEvent.click(removeBtn1);
 
+    userEvent.click(addBtn1);
+    userEvent.click(addBtn1);
     userEvent.click(addBtn2);
-    expect(removeBtn2).toBeEnabled();
-    userEvent.click(removeBtn2);
+
+    expect(checkouBtn).toBeEnabled();
+    userEvent.click(checkouBtn);
   });
 });
